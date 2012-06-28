5a78f6e4755a51018ee77e96a056a5e072a921ac
Marco Paga <marco.paga@gmail.com>
2012-06-28 19:37:58 +0200

    with atoms

--- src/intermezzo/landoflisp.clj
+++ src/intermezzo/landoflisp.clj
@@ -2,21 +2,20 @@
 
 ; Guess my number
 
-(def *upper-bound* 100)
-
-(def *lower-bound* 1)
+(def *upper-bound* (atom 100))
+(def *lower-bound* (atom 1))
 
 (defn guess-my-number[]
-  (int (/ (+ *upper-bound* *lower-bound*) 2)))
+  (int (/ (+ @*upper-bound* @*lower-bound*) 2)))
 
 (defn smaller[]
-  (def *upper-bound* (- (guess-my-number) 1))
+  (reset! *upper-bound* (- (guess-my-number) 1))
   (guess-my-number))
 
 (defn bigger[]
-  (def *lower-bound* (+ (guess-my-number) 1))
+  (reset! *lower-bound* (+ (guess-my-number) 1))
   (guess-my-number))
 
 (defn reset[]
-  (def *upper-bound* 100)
-  (def *lower-bound* 1))
+  (def *upper-bound* (atom 100))
+  (def *lower-bound* (atom 1)))
